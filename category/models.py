852996d57a694e5f024d43af2b680d52cc1fd9a7
Francis Githae <francis.githae@student.moringaschool.com>
2022-02-26 05:09:43 +0300

    add category model

--- category/models.py
+++ category/models.py
@@ -1,3 +1,8 @@
+from unicodedata import category
 from django.db import models
 
 # Create your models here.
+
+class Category(models.Model):
+  category_name=models.Charfield(max_length=100)
+  category_slug_name=models.CharField(max_length=100,blank=True)
