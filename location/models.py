9bf10d1518499e9d40de12ef7316f58ee69a00ea
Francis Githae <francis.githae@student.moringaschool.com>
2022-02-26 09:09:06 +0300

    add methods and tests for the location model

--- location/models.py
+++ location/models.py
@@ -7,3 +7,12 @@ class Location(models.Model):
   location_slug=models.CharField(max_length=200,blank=True)
   location_description=models.CharField(max_length=500)
   created_at=models.DateTimeField(auto_now_add=True)
+
+
+  def save_location(self):
+    """
+    Method that saves a new location
+    Args: 
+        self: a new instance of the location model
+    """
+    self.save()
--- location/tests.py
+++ location/tests.py
@@ -1,3 +1,21 @@
 from django.test import TestCase
 
 # Create your tests here.
+
+from .models import Location
+
+class LocationTestClass(TestCase):
+
+  def setUp(self):
+    self.nairobi=Location(location_name='Nairobi',location_slug='nairobi_city')
+
+  def test_instance(self):
+    self.assertTrue(isinstance(self.nairobi,Location))
+
+  def test_instance_variables(self):
+    self.assertEquals(self.nairobi.location_name,'Nairobi')
+  
+  def test_save_location(self):
+    self.nairobi.save_location()
+    locations=Location.objects.all()
+    self.assertTrue(len(locations)>0)
