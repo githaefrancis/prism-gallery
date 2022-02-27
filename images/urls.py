e9ea381cd832e1477544cac192915a3daf70f247
Francis Githae <francis.githae@student.moringaschool.com>
2022-02-27 12:54:22 +0300

    create gallery.html file

--- images/urls.py
+++ images/urls.py
@@ -1,8 +1,8 @@
-from django.urls import path
+from django.urls import path, re_path
 from . import views
 
 
 urlpatterns=[
   path('',views.index,name='home'),
-
+  re_path(r'^gallery/.*$',views.gallery,name='gallery')
 ]
--- images/views.py
+++ images/views.py
@@ -4,3 +4,7 @@ from django.shortcuts import render
 
 def index(request):
   return render(request,'index.html')
+
+
+def gallery(request):
+  return render(request,'gallery.html')
