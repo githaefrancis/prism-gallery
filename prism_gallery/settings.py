edb74349a303872214ca1ebe286b74ecab545a11
githaefrancis <mureithigithae@gmail.com>
2023-06-22 10:19:01 +0300

    fix(Database)

--- prism_gallery/settings.py
+++ prism_gallery/settings.py
@@ -118,7 +118,7 @@ if config('MODE')=="dev":
             'USER':config('DB_USER'),
             'PASSWORD':config('DB_PASSWORD'),
             'HOST':config('DB_HOST'),
-            'PORT':'',
+            'PORT':'5433',
         }
     }
 
